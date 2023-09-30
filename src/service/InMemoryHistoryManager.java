042ee05c8b561db6b11af003570edf2f8bff633e
lap-lik <9266010166@mail.ru>
2023-09-30 23:44:15 +0300

    style: вынес размер истории просмотра в константу

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -5,12 +5,13 @@ import model.Task;
 import java.util.ArrayList;
 import java.util.List;
 
-public class InMemoryHistoryManager implements HistoryManager {
-    private static final List<Task> history = new ArrayList<>();
 
+public class InMemoryHistoryManager implements HistoryManager {
+    public static final int LIST_SIZE = 9;
+    private static final List<Task> history = new ArrayList<>();
     @Override
     public void add(Task task) {
-        if (history.size() > 9) {
+        if (history.size() > LIST_SIZE) {
             history.remove(0);
         }
             history.add(task);
